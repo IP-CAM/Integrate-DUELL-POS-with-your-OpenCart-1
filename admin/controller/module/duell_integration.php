3851d277541913745e03d74e74c84e3d62f5966c
kgpanteon <kash@panteon.no>
2017-05-19 11:52:32 +0530

    Opencart 1.x duell stock integration

---
+++ admin/controller/module/duell_integration.php
@@ -0,0 +1,211 @@
+<?php
+
+class ControllerModuleDuellIntegration extends Controller {
+
+  private $error = array();
+
+  public function index() {
+    /* Load language file. */
+    $this->load->language('module/duell_integration');
+
+    $this->document->setTitle($this->language->get('heading_title'));
+
+    $this->load->model('setting/setting');
+
+    /* Check if data has been posted back. */
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+      $this->model_setting_setting->editSetting('duell_integration', $this->request->post);
+
+      $this->session->data['success'] = $this->language->get('text_success');
+
+      $this->cache->delete('duell_integration');
+
+      $this->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+    }
+
+    /* Load language strings. */
+    $this->data['text_edit'] = $this->language->get('text_edit');
+
+    $this->data['text_about'] = $this->language->get('text_about');
+    $this->data['text_module'] = $this->language->get('text_module');
+    $this->data['text_enabled'] = $this->language->get('text_enabled');
+    $this->data['text_disabled'] = $this->language->get('text_disabled');
+
+    $this->data['heading_title'] = $this->language->get('heading_title');
+
+    $this->data['entry_title'] = $this->language->get('entry_title');
+    $this->data['entry_text'] = $this->language->get('entry_text');
+    $this->data['entry_status'] = $this->language->get('entry_status');
+
+    $this->data['button_save'] = $this->language->get('button_save');
+    $this->data['button_cancel'] = $this->language->get('button_cancel');
+
+
+    $this->data['text_duell_integration_client_number'] = $this->language->get('text_duell_integration_client_number');
+    $this->data['text_duell_integration_client_token'] = $this->language->get('text_duell_integration_client_token');
+    $this->data['text_duell_integration_department_token'] = $this->language->get('text_duell_integration_department_token');
+
+
+    $this->data['help_text_duell_integration_client_number'] = $this->language->get('help_text_duell_integration_client_number');
+    $this->data['help_text_duell_integration_client_token'] = $this->language->get('help_text_duell_integration_client_token');
+    $this->data['help_text_duell_integration_department_token'] = $this->language->get('help_text_duell_integration_department_token');
+
+    $this->data['text_duell_integration_manual_sync'] = $this->language->get('text_duell_integration_manual_sync');
+
+
+    $this->data['text_user_guide'] = $this->language->get('text_user_guide');
+    $this->data['text_cron_steup_title_curl'] = $this->language->get('text_cron_steup_title_curl');
+    $this->data['text_every_hours'] = $this->language->get('text_every_hours');
+    $this->data['text_every_night'] = $this->language->get('text_every_night');
+
+
+
+    /* Present error messages to users. */
+    if (isset($this->error['warning'])) {
+      $this->data['error_warning'] = $this->error['warning'];
+    } else {
+      $this->data['error_warning'] = '';
+    }
+
+    if (isset($this->error['duell_integration_client_number'])) {
+      $this->data['error_duell_integration_client_number'] = $this->error['duell_integration_client_number'];
+    } else {
+      $this->data['error_duell_integration_client_number'] = '';
+    }
+
+
+    if (isset($this->error['duell_integration_client_token'])) {
+      $this->data['error_duell_integration_client_token'] = $this->error['duell_integration_client_token'];
+    } else {
+      $this->data['error_duell_integration_client_token'] = '';
+    }
+
+    if (isset($this->error['duell_integration_department_token'])) {
+      $this->data['error_duell_integration_department_token'] = $this->error['duell_integration_department_token'];
+    } else {
+      $this->data['error_duell_integration_department_token'] = '';
+    }
+
+    /* Breadcrumb. */
+    $this->data['breadcrumbs'][] = array(
+        'text' => $this->language->get('text_home'),
+        'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL'),
+        'separator' => false
+    );
+
+    $this->data['breadcrumbs'][] = array(
+        'text' => $this->language->get('text_module'),
+        'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'),
+        'separator' => ' :: '
+    );
+
+    $this->data['breadcrumbs'][] = array(
+        'text' => $this->language->get('heading_title'),
+        'href' => $this->url->link('module/duell_integration', 'token=' . $this->session->data['token'], 'SSL'),
+        'separator' => ' :: '
+    );
+
+    $this->data['action'] = $this->url->link('module/duell_integration', 'token=' . $this->session->data['token'], 'SSL');
+
+    $this->data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+
+    $this->data['cron_link'] = HTTP_CATALOG . 'system/duellcron.php';
+
+    $this->data['token'] = $this->session->data['token'];
+
+
+    /* Initial values for form. */
+    if (isset($this->request->post['duell_integration_client_number'])) {
+      $this->data['duell_integration_client_number'] = $this->request->post['duell_integration_client_number'];
+    } else {
+      $this->data['duell_integration_client_number'] = $this->config->get('duell_integration_client_number');
+    }
+
+
+    if (isset($this->request->post['duell_integration_client_token'])) {
+      $this->data['duell_integration_client_token'] = $this->request->post['duell_integration_client_token'];
+    } else {
+      $this->data['duell_integration_client_token'] = $this->config->get('duell_integration_client_token');
+    }
+
+    if (isset($this->request->post['duell_integration_department_token'])) {
+      $this->data['duell_integration_department_token'] = $this->request->post['duell_integration_department_token'];
+    } else {
+      $this->data['duell_integration_department_token'] = $this->config->get('duell_integration_department_token');
+    }
+
+
+
+
+
+    if (isset($this->request->post['duell_integration_status'])) {
+      $this->data['duell_integration_status'] = $this->request->post['duell_integration_status'];
+    } else {
+      $this->data['duell_integration_status'] = $this->config->get('duell_integration_status');
+    }
+
+    /* Render some output. */
+    $this->load->model('design/layout');
+
+    $this->data['layouts'] = $this->model_design_layout->getLayouts();
+
+    $this->template = 'module/duell_integration.tpl';
+    $this->children = array(
+        'common/header',
+        'common/footer'
+    );
+
+    $this->response->setOutput($this->render());
+  }
+
+  /* Check user input. */
+
+  protected function validate() {
+    if (!$this->user->hasPermission('modify', 'module/duell_integration')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
+
+    if (!$this->request->post['duell_integration_client_number'] || strlen($this->request->post['duell_integration_client_number']) <= 5) {
+      $this->error['duell_integration_client_number'] = $this->language->get('error_duell_integration_client_number');
+    }
+
+    if (!$this->request->post['duell_integration_client_token'] || strlen($this->request->post['duell_integration_client_token']) <= 0) {
+      $this->error['duell_integration_client_token'] = $this->language->get('error_duell_integration_client_token');
+    }
+
+    if (!$this->request->post['duell_integration_department_token'] || strlen($this->request->post['duell_integration_department_token']) <= 0) {
+      $this->error['duell_integration_department_token'] = $this->language->get('error_duell_integration_department_token');
+    }
+
+    return !$this->error;
+  }
+
+  public function manualsync() {
+    $json = array();
+
+    $json['status'] = false;
+    $json['message'] = '';
+
+    $this->load->language('module/duell_integration');
+
+    $this->load->library('duell/duell');
+
+    $this->duell = Duell::get_instance($this->registry);
+
+
+
+    $result = $this->duell->callDuellStockSync();
+
+
+    if ($result['status'] == true) {
+      $status = true;
+      $json['message'] = $this->language->get('sync_success');
+    } else {
+      $json['message'] = $result['message'];
+    }
+
+
+    $this->response->setOutput(json_encode($json));
+  }
+
+}
---
+++ admin/view/template/module/duell_integration.tpl
@@ -0,0 +1,162 @@
+<?php
+
+echo $header; ?>
+<div id="content">
+  <div class="breadcrumb">
+        <?php foreach ($breadcrumbs as $breadcrumb) { ?>
+        <?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
+        <?php } ?>
+  </div>
+    <?php if ($error_warning) { ?>
+  <div class="warning"><?php echo $error_warning; ?></div>
+    <?php } ?>
+
+  <div class="box">
+    <div class="heading">
+      <h1><img src="view/image/module.png" alt="" /> <?php echo $heading_title; ?></h1>
+      <div class="buttons"><a onclick="$('#form').submit();" class="button"><?php echo $button_save; ?></a><a href="<?php echo $cancel; ?>" class="button"><?php echo $button_cancel; ?></a></div>
+    </div>
+    <div class="content">
+
+
+
+
+
+
+
+      <form action="<?php echo $action; ?>" method="post" enctype="multipart/form-data" id="form" >
+
+
+
+        <table class="form">
+          <tr>
+            <td><?php echo $text_duell_integration_client_number; ?>
+              <span class="help"><?php echo $help_text_duell_integration_client_number; ?></span>
+            </td>
+            <td><input type="number" name="duell_integration_client_number" value="<?php echo $duell_integration_client_number; ?>" placeholder="<?php echo $text_duell_integration_client_number; ?>" />
+               <?php if ($error_duell_integration_client_number) { ?>
+              <span class="error"><?php echo $error_duell_integration_client_number; ?></span>
+              <?php } ?>
+            </td>
+          </tr>
+
+
+          <tr>
+            <td><?php echo $text_duell_integration_client_token; ?>
+              <span class="help"><?php echo $help_text_duell_integration_client_token; ?></span>
+            </td>
+            <td><input size="50" type="text" name="duell_integration_client_token" value="<?php echo $duell_integration_client_token; ?>" placeholder="<?php echo $text_duell_integration_client_token; ?>" />
+               <?php if ($error_duell_integration_client_token) { ?>
+              <span class="error"><?php echo $error_duell_integration_client_token; ?></span>
+              <?php } ?>
+            </td>
+          </tr>
+
+
+          <tr>
+            <td><?php echo $text_duell_integration_department_token; ?>
+              <span class="help"><?php echo $help_text_duell_integration_department_token; ?></span>
+            </td>
+            <td>
+              <input size="50" type="text" name="duell_integration_department_token" value="<?php echo $duell_integration_department_token; ?>" placeholder="<?php echo $text_duell_integration_department_token; ?>"  />
+               <?php if ($error_duell_integration_department_token) { ?>
+              <span class="error"><?php echo $error_duell_integration_department_token; ?></span>
+              <?php } ?>
+            </td>
+          </tr>
+
+
+
+
+
+          <tr>
+            <td><?php echo $entry_status; ?></td>
+            <td>
+              <select name="duell_integration_status" id="input-status" >
+                <?php if ($duell_integration_status) { ?>
+                <option value="1" selected="selected"><?php echo $text_enabled; ?></option>
+                <option value="0"><?php echo $text_disabled; ?></option>
+                <?php } else { ?>
+                <option value="1"><?php echo $text_enabled; ?></option>
+                <option value="0" selected="selected"><?php echo $text_disabled; ?></option>
+                <?php } ?>
+              </select>
+            </td>
+          </tr>
+
+
+          <tr>
+            <td>&nbsp;</label>
+            </td>
+            <td>
+              <a id="button-syncmanually" data-toggle="tooltip" title="<?php echo $text_duell_integration_manual_sync; ?>" class="button"><?php echo $text_duell_integration_manual_sync; ?></a>
+
+            </td>
+          </tr>
+        </table>
+
+      </form>
+
+
+      <div class="col-sm-12 col-userguide" >
+
+        <p><?php echo $text_user_guide; ?></p>
+
+
+
+        <p>&nbsp;</p>
+        <p>  <b><?php echo $text_cron_steup_title_curl; ?></b></p>
+        <ul  style="list-style-type:circle">
+          <li>
+              <?php echo $text_every_hours; ?>:&nbsp; <b>* */3 * * * /usr/bin/curl <?php echo $cron_link; ?> >/dev/null 2>&1</b>
+          </li>
+          <li>
+              <?php echo $text_every_night; ?>:&nbsp; <b>* 3 * * * /usr/bin/curl <?php echo $cron_link; ?> >/dev/null 2>&1</b>
+          </li>
+        </ul>
+        <p>&nbsp;</p>
+
+
+      </div>
+
+    </div>
+  </div>
+</div>
+<style>
+  .col-userguide{
+    background: #f6f6f6;
+    padding: 10px;
+    margin-bottom: 17px;
+    border: 1px solid #e4e4e4;
+    border-radius: 3px;
+  }
+</style>
+  <script type="text/javascript"><!--
+  $('#button-syncmanually').on('click', function () {
+      $.ajax({
+        url: 'index.php?route=module/duell_integration/manualsync&token=<?php echo $token; ?>',
+        type: 'post',
+        dataType: 'json',
+        cache: false,
+        beforeSend: function () {
+          $('#button-syncmanually').button('loading');
+        },
+        complete: function () {
+          $('#button-syncmanually').button('reset');
+        },
+        success: function (json) {
+          console.log(json);
+          if (json['success']) {
+            alert(json['message']);
+          } else {
+            alert(json['message']);
+          }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+          alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+      });
+
+    });
+    //--></script>
+<?php echo $footer; ?>
