a72b804b02b02d32522d4f29833b2613a384e71a
kgpanteon <kash@panteon.no>
2019-09-04 16:41:35 +0530

    Stock sync API filter added

--- system/library/duell/duell.php
+++ system/library/duell/duell.php
@@ -182,6 +182,7 @@ class Duell {
                 $limit = $this->limit;
 
                 $apiData = array('client_number' => $this->settings['duell_integration_client_number'], 'client_token' => $this->settings['duell_integration_client_token'], 'department' => $this->settings['duell_integration_department_token'], 'length' => $limit, 'start' => $start);
+                $apiData['filter[view_on_webshop]'] = true;
 
                 $wsdata = $this->call('all/product/stock', 'get', $apiData, 'json', $type);
 
@@ -201,7 +202,7 @@ class Duell {
                             while ($totalRecord > $limit && $totalRecord > $nextCounter) {
 
                                 $apiData = array('client_number' => $this->settings['duell_integration_client_number'], 'client_token' => $this->settings['duell_integration_client_token'], 'department_token' => $this->settings['duell_integration_department_token'], 'length' => $limit, 'start' => $nextCounter);
-
+                                $apiData['filter[view_on_webshop]'] = true;
                                 $wsdata = $this->call('all/product/stock', 'get', $apiData, 'json', $type);
 
                                 if ($wsdata['status'] === true) {
